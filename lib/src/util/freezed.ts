386ed5829eabc173c43fe16774ed9b4e4c6a4465
Vinícius Menezes Monte <viniciusmonte2010@gmail.com>
2022-12-01 14:40:13 -0300

    :boom: Angular compatibility drastical measure

--- lib/src/util/freezed.ts
+++ lib/src/util/freezed.ts
@@ -1,6 +1,23 @@
-const Freezed = (target: Function) => {
-  Object.freeze(target);
+const Freezed = (target: any) => {
+  // Object.freeze(target);
+  return new Proxy(target, {
+    construct: function (target, argumentLists, newTarget) {
+      var instance = new target(...argumentLists)
+      Object.freeze(instance)
+      return instance
+    }
+  })
 };
+
+// function freezeInstance(T) {
+//   return new Proxy(T, {
+//     construct: function (target, argumentLists, newTarget) {
+//       var instance = new target(...argumentLists)
+//       Object.freeze(instance)
+//       return instance
+//     }
+//   })
+// }
 
 export { Freezed };
 
