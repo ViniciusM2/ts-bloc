b6b05a10afdd92e1bd099e911df64f892949f1fa
Vinícius Menezes Monte <viniciusmonte2010@gmail.com>
2022-12-02 08:52:47 -0300

    :white_check_mark: Re-introduce tests.

--- lib/src/util/freezed.ts
+++ lib/src/util/freezed.ts
@@ -9,18 +9,6 @@ const Freezed = (target: any) => {
   })
 };
 
-// function freezeInstance(T) {
-//   return new Proxy(T, {
-//     construct: function (target, argumentLists, newTarget) {
-//       var instance = new target(...argumentLists)
-//       Object.freeze(instance)
-//       return instance
-//     }
-//   })
-// }
 
 export { Freezed };
 
-// export const Freezed = (target: Function) => {
-//     Object.freeze(target);
-// };
