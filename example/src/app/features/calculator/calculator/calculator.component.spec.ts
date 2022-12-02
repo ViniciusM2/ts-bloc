71b0e7fdd06cac53f425842e2f5ccd9f5e9fe7f1
Vinícius Menezes Monte <viniciusmonte2010@gmail.com>
2022-12-02 08:55:21 -0300

    :page_facing_up: Re-introduce license

--- example/src/app/features/calculator/calculator/calculator.component.spec.ts
+++ example/src/app/features/calculator/calculator/calculator.component.spec.ts
@@ -22,17 +22,24 @@ describe('CalculatorComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('InitialCalculatorState should be immutable', () => {
-    // Declare a type that undo the 
+  it('should have an immutable initial state', () => {
+    // Declare a type that undo the readonly modifier
     type Writeable<T> = { -readonly [P in keyof T]: T[P] };
+    // Instantiate the supposed immutable object and cast it to the type that undo the readonly modifier
     const state: Writeable<InitialCalculatorState> = new InitialCalculatorState();
+    // Verify the initial state
     expect(state.firstNumber).toBe(0);
+    // Try to mutate the state
     try {
+      // This should throw an error
       state.firstNumber = 1;
     } catch (error) {
-      // Expect a TypeError to be thrown
+      // Expect a TypeError to be thrown. 
+      // This is the error that is thrown when trying to mutate a readonly property. 
+      // It is also the error that is thrown when trying to mutate a frozen object.
       expect(error).toBeInstanceOf(TypeError);
     }
+    // Verify that the state is still the same
     expect(state.firstNumber).toBe(0);
   })
 });
